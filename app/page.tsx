3464afc7be30144f4a6d9b97bbf4cd52aabff055
Miro Ostafiński <ostafinskim@gmail.com>
2024-02-26 21:51:57 +0000

    feat: home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,28 @@
+import Image from 'next/image';
+import { Button } from '../components/ui/button';
+import Link from 'next/link';
+
 export default function Home() {
-    return <h1>hello world</h1>;
+    return (
+        <main className="h-full p-8">
+            <div className="max-w-7xl w-full mx-auto my-14 flex flex-col items-center gap-5">
+                <Image
+                    src={'/gm.svg'}
+                    alt="Speedway bike"
+                    width={1440}
+                    height={100}
+                />
+                <h1 className="text-center text-4xl md:text-6xl lg:text-8xl">Speedway Almanac</h1>
+                <p className="max-w-4xl text-center">Speedway almanac is a collection of data about speedway riders, teams, and tournaments.</p>
+                <div className="flex gap-5">
+                    <Link href="/auth/register">
+                        <Button variant="default">Register</Button>
+                    </Link>
+                    <Link href="/auth/login">
+                        <Button variant="secondary">Login</Button>
+                    </Link>
+                </div>
+            </div>
+        </main>
+    );
 }
